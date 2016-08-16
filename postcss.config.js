bd47841c5624ea4b50ed75bb7f3455fafe8e4f40
Michael Shick <m@shick.us>
2016-08-16 16:21:47 -0700

    postcss config in external file

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'use': [
+    'autoprefixer',
+    'postcss-custom-media',
+    'postcss-media-minmax'
+  ],
+  'autoprefixer': {
+    'browsers': '> 5%'
+  },
+  'postcss-custom-media': true,
+  'postcss-media-minmax': true
+};
