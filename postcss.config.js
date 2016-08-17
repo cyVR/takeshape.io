3cfeffeb9b4a7cada075f37645fea786bd6e0ff2
Michael Shick <m@shick.us>
2016-08-16 19:17:50 -0700

    updating some dev scripts and config

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   'use': [
-    'autoprefixer',
-    'postcss-custom-media',
-    'postcss-media-minmax'
+    'autoprefixer'
   ],
   'autoprefixer': {
     'browsers': '> 5%'
-  },
-  'postcss-custom-media': true,
-  'postcss-media-minmax': true
+  }
 };
