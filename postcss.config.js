20a3fa2a96b7f482704945ca4dea00cc9791b3c9
Michael Shick <m@shick.us>
2016-08-29 07:40:32 -0700

    updating boilerplate

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,21 @@
-module.exports = {
-  'use': [
+'use strict';
+
+const config = {
+  use: [
     'autoprefixer'
   ],
-  'autoprefixer': {
-    'browsers': '> 5%'
+  autoprefixer: {
+    browsers: '> 5%'
+  },
+  'postcss-urlrev': {
+    relativePath: 'assets/stylesheets',
+    absolutePath: __dirname
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.use.push('postcss-urlrev');
+  config.use.push('cssnano');
+}
+
+module.exports = config;
