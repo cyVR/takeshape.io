be43e2fd76cda047a947482aaf7aca5876b0c4c7
Nick Palmucci <nick@new-host-7.home>
2016-09-15 17:51:18 -0400

    js for feature pics and doc menu include

--- _assets/javascripts/test.js
+++ _assets/javascripts/test.js
@@ -1,22 +1,32 @@
-function on() {
-  $('.test-menu').click(function() {
-    $('.sub-menu').css('display', 'block')
-    $('.test-menu').off()
-    offs();
-  })
-}
-function offs() {
-  $('.test-menu').click(function () {
-    $('.sub-menu').css('display', 'none')
-    $('.test-menu').off()
-    on();
-  })
-}
+import ko from "knockout";
 
-$(function(){
+var viewModel = {
 
-  console.log('loadded');
-  on();
+  img1 : {
+  class: ko.observable('shown'),
+  },
+
+  img2 : {
+    class: ko.observable('hidden'),
+  },
+
+  img3 : {
+    class: ko.observable('hidden'),
+  },
+
+  switchImg : function (img) {
+    var arr = [viewModel.img1, viewModel.img2, viewModel.img3]
+
+    for (var i=0; i<3; i++) {
+      arr[i].class('hidden')
+    }
+    img.class('shown');
+
+  }
 
 
-})
+};
+
+
+ko.applyBindings(viewModel);
+
