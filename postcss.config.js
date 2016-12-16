3c55d1daf8cdfe117257c364fda7f8cf8eeab2ac
Andrew Sprouse <sprouse.a@gmail.com>
2016-12-16 11:58:31 -0500

    updated to the latest boilerplate!

--- postcss.config.js
+++ postcss.config.js
@@ -1,32 +1,21 @@
 'use strict';
 
-const fs = require('fs');
-const yaml = require('js-yaml');
-const site = {};// yaml.safeLoad(fs.readFileSync('./_config.yml', 'utf8'));
-
-const config = {
-  use: [
-    'postcss-custom-media',
-    'postcss-media-minmax',
-    'autoprefixer',
-    'lost'
-  ],
-  autoprefixer: {
+let plugins = [
+  require('postcss-custom-media'),
+  require('postcss-media-minmax'),
+  require('autoprefixer')({
     browsers: '> 5%'
-  },
-  'postcss-urlrev': {
-    relativePath: 'assets/stylesheets',
-    absolutePath: __dirname,
-    replacer: (url, hash) => {
-      const baseurl = site.baseurl ? site.baseurl : '';
-      return `${baseurl}${url}?${hash}`;
-    }
-  }
-};
+  }),
+  require('lost')
+];
 
 if (process.env.NODE_ENV === 'production') {
-  config.use.push('postcss-urlrev');
-  config.use.push('cssnano');
+  plugins = plugins.concat([
+    require('postcss-urlrev'),
+    require('cssnano')
+  ]);
 }
 
-module.exports = config;
+module.exports =  {
+  plugins
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
+const webpack = require('webpack');
+const Extract = require('extract-text-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   devtool: isProd ? false : 'source-map',
   entry: {
-    'javascripts/main': './src/javascripts/main.js'
+    'javascripts/main': './src/javascripts/main.js',
+    'stylesheets/main': './src/stylesheets/main.scss'
   },
   output: {
-    path: path.join(__dirname, 'build/assets'),
+    path: path.join(__dirname, 'static/assets'),
     filename: isProd ? '[name].[chunkhash].js' : '[name].js',
   },
   module: {
@@ -17,12 +20,29 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules|vendor/,
         loader: 'babel'
-      }, {
+      },
+      {
+        test: /\.s?css$/,
+        loader:  Extract.extract({
+          fallbackLoader: 'style-loader',
+          loader: [
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        })
+      },
+      {
         test: /\.json$/,
         loader: 'json'
-      }, {
+      },
+      {
         test: /\.html$/,
         loader: 'html'
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        loader: "file-loader?name=[path][name].[hash:base64:5].[ext]"
       }
     ]
   },
@@ -31,6 +51,7 @@ module.exports = {
     extensions: ['.js', '.json']
   },
   plugins: [
-    new ManifestPlugin()
+    new ManifestPlugin(),
+    new Extract(isProd ? '[name].[chunkhash].css' : '[name].css')
   ]
 };
